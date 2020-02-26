415d6cf1831979ab32202246a09ac28e0b5a9809
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-02-26 15:47:19 -0700

    set paths in htmlRoute files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,11 @@ module.exports = function(app) {
     app.get("/notes", function(req,res){
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
-}
+
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    });
+};
 
 
 
